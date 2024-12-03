d88bbe7f98a4dd2e329c54e6afc090d75638c339
Nieterazmamo Czarnas <maticzar1@gmail.com>
2024-12-03 17:35:17 +0000

    Added Ok

--- src/my_project_backend/src/lib.rs
+++ src/my_project_backend/src/lib.rs
@@ -7,17 +7,28 @@ mod blog;
 thread_local! {
     static BLOGS: RefCell<Vec<Blog>> = RefCell::new(Vec::new());
 }
-
+// komentarze ?
 
 #[ic_cdk::update]
-fn add_blog(title: String, content: String, tags: Vec<String>) {
+fn add_blog(title: String, content: String, tags: Vec<String>) -> Result<Blog, String>{
+    if title.len() > 250 {
+        return Err("Title is too long!".to_string())
+    }
     let blog = Blog::new(title, content, tags);
-    BLOGS.with(|blogs|  blogs.borrow_mut().push(blog))
+    BLOGS.with(|blogs| blogs.borrow_mut().push(blog));
+    let last_blog = BLOGS.with(|blogs| 
+        blogs
+        .borrow()
+        .last()
+        .expect("Vec should not be empty")
+        .clone()
+    );
+    Ok(last_blog)
 }
 
 #[ic_cdk::query]
 fn get_blogs() -> Vec<Blog> {
-    BLOGS.with(|blogs|  blogs.borrow().clone())
+    BLOGS.with(|blogs| blogs.borrow().clone())
 }
 
 #[ic_cdk::query]
